9aaa0b2192be7d86fab1f1a0646ae7362752e541
Soailtani <soatianailtani@gmail.com>
2025-03-12 17:57:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Fonction pour suivre les téléchargements
+function trackDownload(fileName) {
+    console.log(`Téléchargement de ${fileName} lancé.`);
+    // Vous pouvez envoyer cette info à votre serveur ou Google Analytics si nécessaire
+    alert(`Vous avez téléchargé: ${fileName}`);
+}
